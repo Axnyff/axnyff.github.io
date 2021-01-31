e7fc29ff4dd24ead614b9271bf20765af215bd55
Axel Nyffenegger <Axnyff@gmail.com>
2021-01-31 19:59:06 +0100

    Remove module.hot.accept en prod

--- minesweeper/src/index.tsx
+++ minesweeper/src/index.tsx
@@ -3,4 +3,6 @@ import { render } from "react-dom";
 import App from "./App";
 
 render(<App />, document.getElementById("app"));
-(module as any).hot.accept();
+if (process.env.NODE_ENV === "development") {
+  (module as any).hot.accept();
+}
