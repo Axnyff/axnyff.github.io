824e3f1f561876930ac07a119f382aee46fbb60a
Axel Nyffenegger <Axnyff@gmail.com>
2021-01-31 18:39:19 +0100

    Webpack initialization done

---
+++ minesweeper/src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => {
+  return <div>YOLO</div>;
+};
+
+render(
+  <App />,
+  document.getElementById('app'),
+)
---
+++ minesweeper/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ["ts-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
+};
